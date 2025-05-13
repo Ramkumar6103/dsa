64fec86aa099dfba067cff854f4322f3cb17a54f
Ramkumar6103 <ramkumarb6103@gmail.com>
2025-05-13 20:11:35 +0530

    Finding 3-Digit Even Numbers

---
+++ findEvenNumbers.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        vector<int> mpp(10, 0);
+        for (int i = 0; i < digits.size(); i++)
+            mpp[digits[i]]++;
+
+        vector<int> res;
+        for (int i = 1; i <= 9; i++) {
+            if (mpp[i] == 0) continue;
+            mpp[i]--;
+
+            for (int j = 0; j <= 9; j++) {
+                if (mpp[j] == 0) continue;
+                mpp[j]--;
+                for (int k = 0; k <= 8; k += 2) {
+                    if (mpp[k] == 0) continue;
+                    res.push_back(i * 100 + j * 10 + k);
+                }
+                mpp[j]++;
+            }
+            mpp[i]++;
+        }
+
+        sort(res.begin(), res.end()); // Sort the results
+        return res;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> digits = {2, 1, 3, 0}; // Example input
+    vector<int> evenNumbers = sol.findEvenNumbers(digits);
+
+    cout << "Even numbers formed using digits: ";
+    for (int num : evenNumbers) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
