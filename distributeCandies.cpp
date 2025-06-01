cb0256fe53a85ec122f966751cf028da89ca76ba
Ramkumar6103 <ramkumarb6103@gmail.com>
2025-06-01 20:49:53 +0530

    Distribute Candies Among Children II

---
+++ distributeCandies.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    // long long distributeCandies(int n, int limit) {          // Approach 1
+    //     long long ans = 0;
+    //     for (int i = 0; i <= limit; i++) {
+    //         for (int j = 0; j <= limit; j++) {
+    //             for (int k = 0; k <= limit; k++) {
+    //                 if ((i + j + k) == n) {
+    //                     ans++;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+        long long distributeCandies(int n, int limit) {
+        long long ans = 0;
+        for(int i=0;i<=limit;i++){
+            for(int j=0;j<=limit;j++){
+                int k = (n-i-j);
+                if(k >= 0 && k <= limit) ans++;
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    int n = 5;
+    int limit = 2;
+
+    Solution sol;
+    long long ways = sol.distributeCandies(n, limit);
+    cout << "Number of ways to distribute candies: " << ways << endl;
+
+    return 0;
+}
