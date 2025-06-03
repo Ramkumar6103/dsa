11596724069541169fb2968b9116748b4dac0d4a
Ramkumar6103 <ramkumarb6103@gmail.com>
2025-06-03 20:50:15 +0530

    Maximum Candies You Can Get from Boxes alternate solution

--- maxCandies.cpp
+++ maxCandies.cpp
@@ -6,57 +6,84 @@ using namespace std;
 
 class Solution {
 public:
-    int maxCandies(vector<int>& status, vector<int>& candies,
-                   vector<vector<int>>& keys, vector<vector<int>>& containedBoxes,
-                   vector<int>& initialBoxes) {
+    // int maxCandies(vector<int>& status, vector<int>& candies,
+    //                vector<vector<int>>& keys, vector<vector<int>>& containedBoxes,
+    //                vector<int>& initialBoxes) {
         
-        int n = status.size();
-        vector<bool> hasBox(n, false);
-        vector<bool> hasKey(n, false);
-        vector<bool> opened(n, false);
+    //     int n = status.size();
+    //     vector<bool> hasBox(n, false);
+    //     vector<bool> hasKey(n, false);
+    //     vector<bool> opened(n, false);
+    //     queue<int> q;
+
+    //     // Add initial boxes
+    //     for (int box : initialBoxes) {
+    //         hasBox[box] = true;
+    //         if (status[box] == 1) {
+    //             q.push(box);
+    //         }
+    //     }
+
+    //     int totalCandies = 0;
+
+    //     while (!q.empty()) {
+    //         int box = q.front();
+    //         q.pop();
+
+    //         if (opened[box]) continue;
+    //         opened[box] = true;
+
+    //         totalCandies += candies[box];
+
+    //         // Add keys found in this box
+    //         for (int key : keys[box]) {
+    //             if (!hasKey[key]) {
+    //                 hasKey[key] = true;
+    //                 if (hasBox[key] && !opened[key]) {
+    //                     q.push(key);
+    //                 }
+    //             }
+    //         }
+
+    //         // Add contained boxes
+    //         for (int newBox : containedBoxes[box]) {
+    //             if (!hasBox[newBox]) {
+    //                 hasBox[newBox] = true;
+    //             }
+    //             if ((status[newBox] == 1 || hasKey[newBox]) && !opened[newBox]) {
+    //                 q.push(newBox);
+    //             }
+    //         }
+    //     }
+
+    //     return totalCandies;
+    // }
+
+    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
+        int cnt=0;
         queue<int> q;
-
-        // Add initial boxes
-        for (int box : initialBoxes) {
-            hasBox[box] = true;
-            if (status[box] == 1) {
-                q.push(box);
+        for(auto it:initialBoxes){ q.push(it); }
+        while(!q.empty()){
+            int sz=q.size();
+            vector<int> curr; vector<int> inserted;
+            while(sz--){
+                int box=q.front(); q.pop();
+                curr.push_back(box);
+                if(status[box]){
+                    cnt+=candies[box]; 
+                    for(auto key:keys[box]){status[key]=1;}
+                    for(auto other:containedBoxes[box]){q.push(other);}
+                }
+                else{
+                    q.push(box);
+                    inserted.push_back(box);
+                }
+            }
+            if(curr==inserted){
+                break;
             }
         }
-
-        int totalCandies = 0;
-
-        while (!q.empty()) {
-            int box = q.front();
-            q.pop();
-
-            if (opened[box]) continue;
-            opened[box] = true;
-
-            totalCandies += candies[box];
-
-            // Add keys found in this box
-            for (int key : keys[box]) {
-                if (!hasKey[key]) {
-                    hasKey[key] = true;
-                    if (hasBox[key] && !opened[key]) {
-                        q.push(key);
-                    }
-                }
-            }
-
-            // Add contained boxes
-            for (int newBox : containedBoxes[box]) {
-                if (!hasBox[newBox]) {
-                    hasBox[newBox] = true;
-                }
-                if ((status[newBox] == 1 || hasKey[newBox]) && !opened[newBox]) {
-                    q.push(newBox);
-                }
-            }
-        }
-
-        return totalCandies;
+        return cnt;
     }
 };
 
