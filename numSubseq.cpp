606a27eb91d95b1c3af318d8628ecfb80d2027fd
Ramkumar6103 <ramkumarb6103@gmail.com>
2025-06-29 22:17:44 +0530

    1498. Number of Subsequences That Satisfy the Given Sum Condition

---
+++ numSubseq.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int numSubseq(vector<int>& nums, int target) {
+        int mod = 1e9 + 7, n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        vector<int> power(n, 1);
+        for (int i = 1; i < n; ++i) {
+            power[i] = (power[i - 1] * 2) % mod;
+        }
+
+        int left = 0, right = n - 1, result = 0;
+
+        while (left <= right) {
+            if (nums[left] + nums[right] <= target) {
+                result = (result + power[right - left]) % mod;
+                ++left;
+            } else {
+                --right;
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+    vector<int> nums = {3, 5, 6, 7};
+    int target = 9;
+
+    Solution sol;
+    int answer = sol.numSubseq(nums, target);
+
+    cout << "Number of valid subsequences: " << answer << endl;
+    return 0;
+}
