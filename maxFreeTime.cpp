bf28f564d3d4e307407ce1c493b2d349e8e9dd9c
Ramkumar6103 <ramkumarb6103@gmail.com>
2025-07-10 00:05:02 +0530

    3439. Reschedule Meetings for Maximum Free Time I another version

--- maxFreeTime.cpp
+++ maxFreeTime.cpp
@@ -3,26 +3,47 @@
 #include <algorithm>
 using namespace std;
 
+// class Solution {
+// public:
+//     int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
+//         int count = startTime.size();
+//         vector<int> prefixSum(count + 1, 0);
+//         int maxFree = 0;
+
+//         for (int i = 0; i < count; ++i) {
+//             prefixSum[i + 1] = prefixSum[i] + (endTime[i] - startTime[i]);
+//         }
+
+//         for (int i = k - 1; i < count; ++i) {
+//             int occupied = prefixSum[i + 1] - prefixSum[i - k + 1];
+//             int windowEnd = (i == count - 1) ? eventTime : startTime[i + 1];
+//             int windowStart = (i == k - 1) ? 0 : endTime[i - k];
+//             int freeTime = windowEnd - windowStart - occupied;
+//             maxFree = max(maxFree, freeTime);
+//         }
+
+//         return maxFree;
+//     }
+// };
+
 class Solution {
 public:
     int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
-        int count = startTime.size();
-        vector<int> prefixSum(count + 1, 0);
-        int maxFree = 0;
-
-        for (int i = 0; i < count; ++i) {
-            prefixSum[i + 1] = prefixSum[i] + (endTime[i] - startTime[i]);
+        int n = startTime.size();
+        vector<int> freeTime;
+        freeTime.push_back(startTime[0]);
+        for(int i=1;i<n;i++){
+            freeTime.push_back(startTime[i] - endTime[i-1]);
         }
-
-        for (int i = k - 1; i < count; ++i) {
-            int occupied = prefixSum[i + 1] - prefixSum[i - k + 1];
-            int windowEnd = (i == count - 1) ? eventTime : startTime[i + 1];
-            int windowStart = (i == k - 1) ? 0 : endTime[i - k];
-            int freeTime = windowEnd - windowStart - occupied;
-            maxFree = max(maxFree, freeTime);
+        freeTime.push_back(eventTime - endTime[n-1]);
+        int currTime = 0;
+        for(int i=0;i<k+1;i++) currTime += freeTime[i];
+        int maxFreeTime = currTime;
+        for(int i=k+1;i<freeTime.size();i++){
+            currTime += freeTime[i] - freeTime[i-(k+1)];
+            maxFreeTime = max(currTime,maxFreeTime);
         }
-
-        return maxFree;
+        return maxFreeTime;
     }
 };
 
