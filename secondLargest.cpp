f058a1c8e7c08106daf191ea05a3f6afc9c54032
Ramkumar6103 <ramkumarb6103@gmail.com>
2025-07-18 11:26:21 +0530

    Second largest updation

--- secondLargest.cpp
+++ secondLargest.cpp
@@ -3,20 +3,24 @@ using namespace std;
 class Solution{
     public:
     int secondLargest(vector<int>& arr){
-        int firstLL = arr[0];
-        int secondLL = arr[0];
-        for(int i=1;i<arr.size();i++){
+        if(arr.size() <= 1) return -1;
+        int firstLL = INT_MIN;
+        int secondLL = INT_MIN;
+        for(int i=0;i<arr.size();i++){
             if(arr[i] > firstLL){
                 secondLL = firstLL;
                 firstLL = arr[i];
-            } 
+            }else if(arr[i] > secondLL && arr[i] != firstLL) secondLL = arr[i];
+        }
+        if(secondLL == INT_MIN){
+            return firstLL;
         }
         return secondLL;
     }
 };
 int main(){
     Solution obj;
-    vector<int> arr = {4,5,3,1,2};
+    vector<int> arr = {1,2,5,7,6,4};
     cout<<"Second largest element is "<<obj.secondLargest(arr);
     return 0;
 }
