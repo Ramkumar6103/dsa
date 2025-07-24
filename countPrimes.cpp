e259009fdc348ee2c90471ea5c71fcbfe7a56591
Ramkumar6103 <ramkumarb6103@gmail.com>
2025-07-24 21:05:10 +0530

    Optimist way approach

--- countPrimes.cpp
+++ countPrimes.cpp
@@ -4,23 +4,38 @@ using namespace std;
 
 class Solution {
 public:
-    int countPrimes(int n) {
-        int cnt = 0;
-        if(n <= 1) return 0;
+    // int countPrimes(int n) {
+    //     int cnt = 0;
+    //     if(n <= 1) return 0;
 
-        for(int i = 2; i < n; i++) {
-            bool isPrime = true;
-            for(int j = 2; j <= sqrt(i); j++) {  // fixed condition & increment
-                if(i % j == 0) {
-                    isPrime = false;
-                    break;
+    //     for(int i = 2; i < n; i++) {
+    //         bool isPrime = true;
+    //         for(int j = 2; j <= sqrt(i); j++) {  // fixed condition & increment
+    //             if(i % j == 0) {
+    //                 isPrime = false;
+    //                 break;
+    //             }
+    //         }
+    //         if(isPrime) cnt++;
+    //     }
+    //     return cnt;
+    // }
+
+        int countPrimes(int n) {
+        int cnt = 0;
+        vector<bool> prime(n + 1, true);
+        prime[0] = prime[1] = false;
+        for (int i = 2; i < n; i++) {
+            if (prime[i]) {
+                cnt++;
+                for (int j = i * 2; j < n; j += i) {
+                    prime[j] = false;
                 }
             }
-            if(isPrime) cnt++;
         }
-
         return cnt;
     }
+
 };
 
 int main() {
