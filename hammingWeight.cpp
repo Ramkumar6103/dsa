e777314db4566dfd9e4dce2a2db49f3a8e56640d
Ramkumar6103 <ramkumarb6103@gmail.com>
2025-09-10 20:22:37 +0530

    191. Number of 1 Bits

---
+++ hammingWeight.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdint>
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n >> i) & 1) {
+                res += 1;
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    // Example input
+    uint32_t input = 11; // Binary: 00000000000000000000000000001011
+    int result = sol.hammingWeight(input);
+
+    cout << "Hamming weight of " << input << " is: " << result << endl;
+
+    return 0;
+}
