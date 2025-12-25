bb1f0b52de8ce56e9925116742267663794e3028
Ramkumar6103 <ramkumarb6103@gmail.com>
2025-12-25 23:05:16 +0530

    Can Sum DP

---
+++ canSum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool canSum(vector<int>& nums, int targetSum) {
+        vector<bool> dp(targetSum + 1, false);
+        dp[0] = true;
+
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = nums[i]; j <= targetSum; j++) {
+                // cout<<j-nums[i]<<" ";
+                if (dp[j - nums[i]]) {
+                    dp[j] = true;
+                }
+            }
+        }
+
+        return dp[targetSum];
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {5, 3, 4};
+    int targetSum = 7;
+    cout<<sol.canSum(nums, targetSum);
+    return 0;
+}
