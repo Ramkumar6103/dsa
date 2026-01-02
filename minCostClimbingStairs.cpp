dcab422195ef4ccef60b530a8c442cbebc8a52cd
Ramkumar6103 <ramkumarb6103@gmail.com>
2026-01-02 21:36:07 +0530

    746. Min Cost Climbing Stairs

---
+++ minCostClimbingStairs.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int minCostClimbingStairs(vector<int>& cost) {
+    int n = cost.size();
+    vector<int> dp(n + 1);
+
+    dp[0] = 0;
+    dp[1] = 0;
+
+    for (int i = 2; i <= n; i++) {
+        dp[i] = min(
+            dp[i - 1] + cost[i - 1],
+            dp[i - 2] + cost[i - 2]
+        );
+    }
+
+    return dp[n];
+}
+
+int main() {
+    vector<int> cost = {10, 15, 20};  // hardcoded input
+
+    cout << "Minimum cost to reach top: ";
+    cout << minCostClimbingStairs(cost);
+
+    return 0;
+}
